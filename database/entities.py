38b82c7d7ebcaebd37d098c92d914875118967da
semyon danshin <semen.danshin@gmail.com>
2024-08-01 03:54:52 +0300

    db: Think about nullable

--- database/entities.py
+++ database/entities.py
@@ -24,20 +24,20 @@ class Worker(BaseEntity):
     app_hash: Mapped[str] = mapped_column(String, nullable=False)
     session_string: Mapped[str] = mapped_column(String, nullable=False)
     proxy: Mapped[str] = mapped_column(String, nullable=True)
-    campaign_id: Mapped[str] = mapped_column(UUID(as_uuid=True), ForeignKey('campaigns.id'), nullable=False)
-    role: Mapped[str] = mapped_column(String, nullable=False)
-    status: Mapped[str] = mapped_column(String, nullable=False)
+    campaign_id: Mapped[str] = mapped_column(UUID(as_uuid=True), ForeignKey('campaigns.id'), nullable=True)
+    role: Mapped[str] = mapped_column(String, nullable=False, server_default='parser')
+    status: Mapped[str] = mapped_column(String, nullable=False, server_default='stoped')
     username: Mapped[str] = mapped_column(String, nullable=False)
-    bio: Mapped[str] = mapped_column(String)
+    bio: Mapped[str] = mapped_column(String, nullable=True)
 
 
 class GPT(BaseEntity):
     __tablename__ = 'gpt_settings'
 
     model: Mapped[str] = mapped_column(String, nullable=False)
-    assistant: Mapped[str] = mapped_column(String, nullable=False)
+    assistant: Mapped[str] = mapped_column(String, nullable=True)
     token: Mapped[str] = mapped_column(String, nullable=False)
-    service_prompt: Mapped[str] = mapped_column(String, nullable=False, default='')
+    service_prompt: Mapped[str] = mapped_column(String, nullable=True)
 
 
 class Campaign(BaseEntity):
@@ -61,6 +61,7 @@ class Chat(BaseEntity):
     status: Mapped[str] = mapped_column(String, nullable=False)
     lead_message: Mapped[str] = mapped_column(String, nullable=False)
     lead_chat_id: Mapped[str] = mapped_column(String, nullable=False)
+    auto_reply: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default='true')
 
 
 class Message(BaseEntity):
