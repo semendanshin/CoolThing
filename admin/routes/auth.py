b1137b61ef52c4f7cb814cc0e3412a0d06cca54f
semyon danshin <semen.danshin@gmail.com>
2024-08-03 18:51:18 +0300

    feat: Make auth form powered by javascript. Add destination parma to auth page

--- admin/routes/auth.py
+++ admin/routes/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from starlette.requests import Request
-from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse, JSONResponse
 from starlette.templating import Jinja2Templates
 
 from abstractions.usecases.AuthUseCaseInterface import AuthUseCaseInterface
@@ -20,24 +20,27 @@ templates = Jinja2Templates(directory='templates')
 @router.get("")
 async def get_login_page(
         request: Request,
+        destination: str = "/dashboard",
 ):
     return templates.TemplateResponse(
         request=request,
         name='auth.html',
-        context={},
+        context={
+            "destination": destination
+        },
     )
 
 
 @router.post("")
-async def get_auth_code(
+async def validate_auth_code_backend(
         credentials: Credentials = Depends(auth_code_form),
         auth_use_case: AuthUseCaseInterface = Depends(get_auth_use_case),
-):
+) -> JSONResponse:
     try:
         tokens = await auth_use_case.create_tokens(credentials)
-        response = RedirectResponse(url='/dashboard', status_code=303)
+        response = JSONResponse(content={"status": "ok"}, status_code=200)
         response.set_cookie(key="access_token", value=tokens.access_token)
         # response.set_cookie(key="refresh_token", value=tokens.refresh_token)
         return response
     except WrongCredentialsException:
-        return RedirectResponse(url='/auth', status_code=303)
+        return JSONResponse(content={"status": "error", "message": "Wrong credentials"}, status_code=400)
