86a878f628dbfde71666ed7e3943d732ab6e3366
firs-iln <firs_iln@mail.ru>
2024-08-09 02:22:04 +0300

    add to db

--- database/entities.py
+++ database/entities.py
@@ -15,6 +15,7 @@ class BaseEntity(Base):
     id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False, server_default=func.now(), onupdate=func.current_timestamp())
+    deleted_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=True, index=True, server_default=None)
 
 
 class Worker(BaseEntity):
