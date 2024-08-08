d24045535c75e1fe3cb4993cc9a6b9a094346ad9
semyon danshin <semen.danshin@gmail.com>
2024-08-08 20:53:05 +0300

    feat: Add intervals to campaign create form.

--- admin/forms/campaign_create.py
+++ admin/forms/campaign_create.py
@@ -6,12 +6,16 @@ from domain.dto.campaign import CampaignCreateDTO
 
 
 def create_campaign_form(
-    welcome_message: str = Form(...),
-    chats: str = Form(...),
-    plus_keywords: str = Form(...),
-    minus_keywords: str = Form(...),
-    gpt_settings_id: str = Form(...),
-    scope: str = Form(...),
+        welcome_message: str = Form(...),
+        chats: str = Form(...),
+        plus_keywords: str = Form(...),
+        minus_keywords: str = Form(...),
+        gpt_settings_id: str = Form(...),
+        scope: str = Form(...),
+        campaign_chat_interval_start: int = Form(default=None),
+        campaign_chat_interval_end: int = Form(default=None),
+        campaign_welcome_wait_start: int = Form(default=None),
+        campaign_welcome_wait_end: int = Form(default=None),
 ) -> CampaignCreateDTO:
     return CampaignCreateDTO(
         welcome_message=welcome_message,
@@ -20,4 +24,8 @@ def create_campaign_form(
         minus_keywords=[keyword for keyword in re.split(r"\s*,\s*", minus_keywords)],
         gpt_settings_id=gpt_settings_id,
         scope=scope,
+        chat_answer_wait_interval_seconds=(f"{campaign_chat_interval_start}-{campaign_chat_interval_end}"
+                                           if campaign_chat_interval_start and campaign_chat_interval_end else None),
+        new_lead_wait_interval_seconds=(f"{campaign_welcome_wait_start}-{campaign_welcome_wait_end}"
+                                        if campaign_welcome_wait_start and campaign_welcome_wait_end else None),
     )
