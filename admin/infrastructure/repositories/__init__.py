2eb92247667a0ad9fc38ff31c53ae44cf78a1351
firs-iln <firs_iln@mail.ru>
2024-08-08 23:08:51 +0300

    fix: add abstract repo entity attribute

--- admin/infrastructure/repositories/__init__.py
+++ admin/infrastructure/repositories/__init__.py
@@ -17,6 +17,9 @@ class AbstractSQLAlchemyRepository[Entity, Model, CreateDTO, UpdateDTO](
 ):
     session_maker: async_sessionmaker
 
+    def __post_init__(self):
+        self.entity = self.__orig_bases__[0].__args__[0]
+
     async def create(self, obj: CreateDTO) -> None:
         async with self.session_maker() as session:
             async with session.begin():
@@ -25,24 +28,23 @@ class AbstractSQLAlchemyRepository[Entity, Model, CreateDTO, UpdateDTO](
     async def get(self, obj_id: str) -> Model:
         async with self.session_maker() as session:
             # noinspection PyUnresolvedReferences
-            return self.entity_to_model(await session.get(self.__orig_bases__[0].__args__[0], obj_id))
+            return self.entity_to_model(await session.get(self.entity, obj_id))
 
     async def update(self, obj_id: str, obj: UpdateDTO) -> None:
         async with self.session_maker() as session:
             async with session.begin():
-                entity = await session.get(self.__orig_bases__[0].__args__[0], obj_id)
+                entity = await session.get(self.entity, obj_id)
                 for key, value in obj.__dict__.items():
                     setattr(entity, key, value)
 
     async def delete(self, obj_id: str) -> None:
         async with self.session_maker() as session:
             async with session.begin():
-                await session.delete(await session.get(Entity, obj_id))
+                await session.delete(await session.get(self.entity, obj_id))
 
     async def get_all(self, limit: int = 100, offset: int = 0) -> list[Model]:
-        entity = self.__orig_bases__[0].__args__[0]
         async with self.session_maker() as session:
-            result = await session.execute(select(entity).limit(limit).offset(offset))
+            result = await session.execute(select(self.entity).limit(limit).offset(offset))
             return [self.entity_to_model(entity) for entity in
                     result.scalars().all()]
 
