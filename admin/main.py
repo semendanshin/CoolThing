a16bc77485931c0ae02f9d27b4cb7a04dc3cd0b5
semyon danshin <semen.danshin@gmail.com>
2024-10-30 14:03:26 +0300

    feat: Add grafana, grafana-loki. Add loki plugin to fluentd. Add dashboard for grafana. Add reverse proxy. Change admin logging format.

--- admin/main.py
+++ admin/main.py
@@ -1,11 +1,16 @@
+import logging
+import sys
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from middlewares import check_for_auth
 from routes import (dashboard_router, bots_router, bot_router, fallback_router, chats_router,
                     campaigns_router, gpt_settings_router, auth_router)
 
-from middlewares import check_for_auth
 
 app = FastAPI()
 
@@ -24,4 +29,4 @@ app.include_router(gpt_settings_router)
 app.include_router(auth_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8080, log_config="logging.json")
