3f2428b05cc45e773d8d43f8a83c3e398400d1ca
daria021 <dariaedigareva@gmail.com>
2025-02-28 15:50:58 +0300

    fix

--- metrics_worker/main.py
+++ metrics_worker/main.py
@@ -10,12 +10,16 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
+#
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     # Инициализация БД
+#     await init_db()
+#     yield  # После yield FastAPI продолжит работу, а при завершении приложения — выполнит завершающую часть (если нужна)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Инициализация БД
+app = FastAPI() #(lifespan=lifespan)
+
+@app.on_event("startup")
+async def startup_event():
     await init_db()
-    yield  # После yield FastAPI продолжит работу, а при завершении приложения — выполнит завершающую часть (если нужна)
-
-app = FastAPI(lifespan=lifespan)
 app.include_router(router)
