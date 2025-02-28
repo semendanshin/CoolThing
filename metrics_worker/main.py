bb9cd2276f6852770dd73db1b6478c4eb430e5ff
daria021 <dariaedigareva@gmail.com>
2025-02-28 14:52:21 +0300

    fix

--- metrics_worker/main.py
+++ metrics_worker/main.py
@@ -1,6 +1,13 @@
+import logging
+
 from fastapi import FastAPI
 
 from metrics.routes import router
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 app = FastAPI()
 app.include_router(router)
