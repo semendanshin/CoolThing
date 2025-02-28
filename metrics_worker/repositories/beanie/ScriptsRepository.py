f00befefb4d06edf6d4b94153328c1600184465d
daria021 <dariaedigareva@gmail.com>
2025-02-28 15:09:14 +0300

    fix

--- metrics_worker/repositories/beanie/ScriptsRepository.py
+++ metrics_worker/repositories/beanie/ScriptsRepository.py
@@ -20,7 +20,9 @@ class ScriptsRepository(
         threshold = datetime.now() - timedelta(days=n)
         logger.info("лол")
         logger.info(self.entity.__dict__)
+        logger.info("тут")
         logger.info(self.entity.find_all())
+        logger.info("там")
         # Фильтруем по дате создания
         scripts = await self.entity.find(self.entity.created_at >= threshold).to_list()
         return [self.entity_to_model(script) for script in scripts]
