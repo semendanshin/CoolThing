23f47d343d7ce4e9d049da699e97ff6538ddea2c
daria021 <dariaedigareva@gmail.com>
2025-02-28 16:18:13 +0300

    fix

--- metrics_worker/repositories/beanie/__init__.py
+++ metrics_worker/repositories/beanie/__init__.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 async def init_db():
     try:
         connection_string = settings.scripts_db.url
+        logger.info(f"Connecting to MongoDB using: {connection_string}")
         client = AsyncIOMotorClient(connection_string)
         await init_beanie(
             database=client[settings.scripts_db.name],
