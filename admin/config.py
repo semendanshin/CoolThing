5c0ea1a96150c4bb4aaf692b8bc866b5971b4d8e
firs-iln <firs_iln@mail.ru>
2025-03-20 16:42:14 +0300

    fix: mongo

--- admin/config.py
+++ admin/config.py
@@ -31,7 +31,7 @@ class ScriptsDBSettings(BaseSettings):
 
     @property
     def url(self):
-        return f"mongodb://{self.user}:{self.password.get_secret_value()}@{self.host}:{self.port}/{self.name}?authSource=admin&directConnection=true"
+        return f"mongodb://{self.user}:{self.password.get_secret_value()}@{self.host}:{self.port}/{self.name}"
 
 
 class AuthSettings(BaseSettings):
