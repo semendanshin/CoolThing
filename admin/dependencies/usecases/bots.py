a989344cafa0cab6d7aebc7c406ba7976399b1f2
firs-iln <firs_iln@mail.ru>
2025-03-20 20:26:31 +0300

    fix: missing dependency

--- admin/dependencies/usecases/bots.py
+++ admin/dependencies/usecases/bots.py
@@ -1,5 +1,6 @@
 from abstractions.usecases import BotsUseCaseInterface
 from dependencies.repositories.bot_session import get_bots_sessions_repository
+from dependencies.repositories.scripts import get_script_for_campaign_repository
 from dependencies.repositories.workers import get_workers_repository
 from usecases.BotsUseCase import BotsUseCase
 
@@ -10,4 +11,5 @@ def get_bots_usecase() -> BotsUseCaseInterface:
     return BotsUseCase(
         workers_repo=get_workers_repository(),
         session_repo=session_repo_maker(),
+        active_scripts_repo=get_script_for_campaign_repository(),
     )
